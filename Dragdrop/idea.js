84e03e415ce6e2f2bc4e81da9a04babfbdc91f1e
perkristianv <pkvinje@hotmail.com>
2019-05-30 16:22:56 +0200

    Enable idea board

--- Dragdrop/idea.js
+++ Dragdrop/idea.js
@@ -1,13 +1,13 @@
 // Open / close idea board
 $(document).ready(function(){
-    $(".idea-board-btn").click(function(){
-      $(".idea-window").toggle();
+    $('#idea-board-btn').click(function(){
+      $('#idea-window').toggle();
     });
   });
 
 // Create draggable idea element
 $(function() {
-    $('.new-idea-btn').on("click", function(){
+    $('#new-idea-btn').on("click", function(){
         var newDiv = $('<div class="idea-content" contentEditable="true"></div>');
     $(".idea-container").append(newDiv);
     $( ".idea-content" ).draggable();
