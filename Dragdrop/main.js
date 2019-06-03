90a56cdfc2392f23af6bebf35b0627d67e9364de
grojon18 <jonas.gronnerod@gmail.com>
2019-06-03 12:54:44 +0200

    Small changes

--- Dragdrop/main.js
+++ Dragdrop/main.js
@@ -19,21 +19,16 @@ dragula([
     });
 
     
-    
-    function addTask() {
-      /* Get task text from input */
-      var inputTask = document.getElementById("taskText").value;
-      if(document.getElementById("taskText").value === ""){
-    document.getElementById("add").disabled = true;
-    }
-    else {
-      /* Add task to the 'To Do' column */
-      document.getElementById("to-do").innerHTML +=
-        "<li class='task'><p>" + inputTask + "</p></li>";
-      /* Clear task text from input after adding task */
-      document.getElementById("taskText").value = "";
-    }
-    }
+/* Vanilla JS to add a new task */
+  function addTask() {
+  /* Get task text from input  */
+  var inputTask = document.getElementById("taskText").value;
+  /* Add task to the 'To Do' column  */
+  document.getElementById("to-do").innerHTML +=
+    "<li class='task' contentEditable='true'><p>" + inputTask + "</p></li>";
+  /* Clear task text from input after adding task */
+  document.getElementById("taskText").value = "";
+}
 
   /* Vanilla JS to delete tasks in 'Trash' column */
   function emptyTrash() {
