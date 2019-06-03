a5f9ebcedfeadba304d62e0e18dc6b340276f5c9
perkristianv <pkvinje@hotmail.com>
2019-06-03 13:33:54 +0200

    Added colorblind button/colors. Added prompt to delete trash

--- Dragdrop/colorblind.js
+++ Dragdrop/colorblind.js
@@ -1,17 +1,17 @@
 // Change color
 
-$('#change-color-btn :checkbox').change(function () {
+$('#color-switch :checkbox').change(function () {
     if ($(this).is(':checked')) {
-        $('.to-do-column .column-header').css('background', 'yellow');
-        $('.doing-column .column-header').css('background', 'pink');
-        $('.done-column .column-header').css('background', 'orange');
-        $('.trash-column .column-header').css('background', 'black');
-        $('.delete-button').css('background', 'blue');
+        $('.to-do-column .column-header').css('background', '#ac9d00');
+        $('.doing-column .column-header').css('background', '#0666e0');
+        $('.done-column .column-header').css('background', '#dfcd06');
+        $('.trash-column .column-header').css('background', '#625900');
+        $('.delete-button').css('background', '#625900');
     } else {
         $('.to-do-column .column-header').css('background', '#ff872f');   
         $('.doing-column .column-header').css('background', '#13a4d9');
         $('.done-column .column-header').css('background', '#15d072');
         $('.trash-column .column-header').css('background', '#ff4444');
-        $('.delete-button').css('background', '#fd4141');
+        $('.delete-button').css('background', '#ff4444');
     }
 });
--- Dragdrop/idea.js
+++ Dragdrop/idea.js
@@ -1,43 +1,44 @@
 // Open / close idea board
-$(document).ready(function(){
-    $('#idea-board-btn').click(function(){
-      $('#idea-window').toggle();
+$(document).ready(function() {
+  $("#idea-board-btn").click(function() {
+    $("#idea-window").toggle();
 
-      var $this = $(this);
-    $this.toggleClass('idea-board-btn');
-    if($this.hasClass('idea-board-btn')){
-        $this.text('Close Board');         
+    var $this = $(this);
+    $this.toggleClass("idea-board-btn");
+    if ($this.hasClass("idea-board-btn")) {
+      $this.text("Close Board");
     } else {
-        $this.text('Idea Board');
+      $this.text("Idea Board");
     }
-    });
   });
-
+});
 
 // Create draggable idea element
 $(function() {
-    $('#new-idea-btn').on("click", function(){
-        var newDiv = $('<div class="idea-content" contentEditable="true" data-text="Enter New Idea . . ."></div>');
+  $("#new-idea-btn").on("click", function() {
+    var newDiv = $(
+      '<div class="idea-content" contentEditable="true" data-text="Enter new idea . . ."></div>'
+    );
     $(".idea-container").append(newDiv);
-    $( ".idea-content" ).draggable();
+    $(".idea-content").draggable();
     $(".idea-content").bind("keydown", function(event) {
       var target = $(event.target);
       c = event.keyCode;
       // Enter og esc to drop focus
-      if(c === 13 || c === 27) {
-          $(".idea-content").blur(); 
+      if (c === 13 || c === 27) {
+        $(".idea-content").blur();
       }
     });
-    });
+  });
 });
 
 // Delete item on trash can drop
 $(function() {
   $(".idea-content").draggable();
 
-  $('#idea-trash').droppable({
-      drop: function(event, ui) {
-          $(ui.draggable).remove();
-      }
+  $("#idea-trash").droppable({
+    drop: function(event, ui) {
+      $(ui.draggable).remove();
+    }
   });
 });
