976dabbc13c8c16a249edb072b24ccdba52e3cd1
Thomas Michelsengit config --global user.email thomas__michelsen@hotmail.comgit config --global user.name = mictho18 <thomas__michelsen@hotmail.com>
2019-06-05 14:28:03 +0200

    Added script for login validation and centered footer.

---
+++ Nova/Assets/Login/JS Scripts/Login_Main.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var un = document.loginForm.usr.value;
+    var pw = document.loginForm.pword.value;
+    var username = "John Doe"; 
+    var password = "password";
+    if ((un == username) && (pw == password)) {
+        return true;
+    }
+    else {
+        alert ("Login was unsuccessful, please check your username and password");
+        return false;
+    }
+}
